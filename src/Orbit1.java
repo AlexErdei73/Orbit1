1114961ccd5fcbaa23d05fd776b76fa711011ef2
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-01-13 23:54:19 +0000

    Output totalEnergy

--- src/Orbit1.java
+++ src/Orbit1.java
@@ -1,6 +1,12 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Orbit1 {
+    double totalEnergy;
     Orbit1(double x0, double y0, double vx0, double vy0, double dt) {
         Plot orbit = new Plot("Orbit", -4, 4, 0.2, -4, 4, 0.2);
+        orbit.setPointSize(1);
+        DecimalFormat fourDecimalDigits = new DecimalFormat("0.0000");
         double tMax = 7;
         double t = 0;
         double x, y, vx, vy, ax, ay;
@@ -8,6 +14,9 @@ public class Orbit1 {
         y = y0;
         vx = vx0;
         vy = vy0;
+        this.totalEnergy = this.totalE(x, y, vx, vy);
+        System.out.print("Energy: ");
+        System.out.println(fourDecimalDigits.format(this.totalEnergy));
         while(t < tMax) {
             ax = this.ax(x, y);
             ay = this.ay(x, y);
@@ -17,7 +26,10 @@ public class Orbit1 {
             vy += ay * dt;
             t += dt;
             orbit.addPoint(x, y);
+            this.totalEnergy = this.totalE(x, y, vx, vy);
         }
+        System.out.print("Energy: ");
+        System.out.println(fourDecimalDigits.format(this.totalEnergy));
     }
 
     private double ax(double x, double y) {
@@ -29,7 +41,14 @@ public class Orbit1 {
         double G = 4 * Math.PI * Math.PI;
         return -G * y * Math.pow((x * x + y * y), -1.5);
     }
+
+    private double totalE(double x, double y, double vx, double vy) {
+        double G = 4 * Math.PI * Math.PI;
+        double kinE = 0.5 * (vx * vx + vy * vy);
+        double potE = - G / Math.sqrt(x * x + y * y);
+        return kinE + potE;
+    }
     public static void main(String[] args) {
-        new Orbit1(1.5, 0, 0, 2 * Math.PI / Math.sqrt(1.5), 0.01);
+        new Orbit1(1.5, 0, 0, 2 * Math.PI / Math.sqrt(1.5), 0.001);
     }
 }
